2c9ecdb4b578402015c5b5106ba9f5249fd656e8
ashrafulhossain1 <ahmeadashraful@gmail.com>
2026-04-07 05:04:45 +0600

    new branch

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -14,7 +14,10 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <Link href="/" className="font-display font-bold text-xl text-navy-DEFAULT tracking-wide">
+          <Link
+            href="/"
+            className="font-display font-bold text-xl text-navy-DEFAULT tracking-wide"
+          >
             EXPOVIVIENDA
           </Link>
 
